f945a72152a4615b17b02e552d28f63712d48655
Avani Patel <anpatel36@yahoo.in>
2021-08-11 15:44:10 -0400

    Getting Permissions

--- App.js
+++ App.js
@@ -1,9 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, {useEffect} from 'react';
 import { StyleSheet, Button, Text, View } from 'react-native';
 import * as Notifications from 'expo-notifications';
+import * as Permissions from 'expo-permissions';
 
 export default function App() {
+
+  useEffect(() => {
+    Permissions.getAsync(Permissions.NOTIFICATIONS).then(statusObj => {
+      if(statusObj.status !== 'granted') {
+        return Permissions.askAsync(Permissions.NOTIFICATIONS);
+      }
+      return statusObj;
+    }).then(statusObj => {
+      if(statusObj.status !== 'granted') {
+        return;
+      }
+    });
+  }, [])
 
   const triggerNotificationHandler = () => {
     Notifications.scheduleNotificationAsync({
