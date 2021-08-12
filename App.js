7ac62d610b07464276f22c3eb5654b56404f2c00
Avani Patel <anpatel36@yahoo.in>
2021-08-12 13:46:37 -0400

    Expo and Notifications

--- App.js
+++ App.js
@@ -22,9 +22,13 @@ export default function App() {
       return statusObj;
     }).then(statusObj => {
       if(statusObj.status !== 'granted') {
-        return;
+        throw new Error('Permission not granted!');
       }
-    });
+    }).then(() => {
+
+    }).catch(err => {
+      return null;
+    })
   }, [])
 
   useEffect(() => {
@@ -44,16 +48,16 @@ export default function App() {
   }, []);
 
   const triggerNotificationHandler = () => {
-    Notifications.scheduleNotificationAsync({
-      content: {
-        title: "My first local notification.",
-        body: "This is the first local notification we are sending!",
-        data: { mySpecialData: 'Some text' }
-      },
-      trigger: {
-        seconds: 10,
-      }
-    });
+    // Notifications.scheduleNotificationAsync({
+    //   content: {
+    //     title: "My first local notification.",
+    //     body: "This is the first local notification we are sending!",
+    //     data: { mySpecialData: 'Some text' }
+    //   },
+    //   trigger: {
+    //     seconds: 10,
+    //   }
+    // });
   }
 
   return (
